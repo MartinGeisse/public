0b6c8e848543a7bc58a32f9753f3667b3ec06139
Martin <martin@Martins-Mac-mini.local>
2012-06-04 22:34:53 +0200

    ...

---
+++ webapp.admin/src/name/martingeisse/admin/multi/EntityInstanceDataProvider.java
@@ -0,0 +1,160 @@
+/**
+ * Copyright (c) 2011 Martin Geisse
+ *
+ * This file is distributed under the terms of the MIT license.
+ */
+
+package name.martingeisse.admin.multi;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import name.martingeisse.admin.schema.AbstractDatabaseDescriptor;
+import name.martingeisse.admin.schema.EntityDescriptor;
+import name.martingeisse.admin.single.EntityInstance;
+import name.martingeisse.common.jdbc.ResultSetReader;
+
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
+/**
+ * An {@link IDataProvider} that fetches entity instances and returns
+ * them as {@link EntityInstance} objects.
+ */
+public class EntityInstanceDataProvider implements IDataProvider<EntityInstance> {
+
+	/**
+	 * the entityModel
+	 */
+	private final IModel<EntityDescriptor> entityModel;
+	
+	/**
+	 * Constructor.
+	 * @param entityModel the model that provides the entity descriptor
+	 */
+	public EntityInstanceDataProvider(IModel<EntityDescriptor> entityModel) {
+		this.entityModel = entityModel;
+	}
+	
+	/**
+	 * Getter method for the entityModel.
+	 * @return the entityModel
+	 */
+	public IModel<EntityDescriptor> getEntityModel() {
+		return entityModel;
+	}
+	
+	/**
+	 * Getter method for the entity.
+	 * @return the entity
+	 */
+	public EntityDescriptor getEntity() {
+		return getEntityModel().getObject();
+	}
+	
+	/**
+	 * This method can be implemented by subclasses to trigger additional behavior
+	 * when the fetch result is available. The default implementation does nothing.
+	 * @param reader the result set reader
+	 */
+	protected void onResultAvailable(ResultSetReader reader) throws SQLException {
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.apache.wicket.model.IDetachable#detach()
+	 */
+	@Override
+	public void detach() {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.wicket.markup.repeater.data.IDataProvider#size()
+	 */
+	@Override
+	public int size() {
+		Connection connection = null;
+		try {
+			final EntityDescriptor entity = getEntity();
+			final AbstractDatabaseDescriptor database = entity.getDatabase();
+			connection = database.createConnection();
+			final Statement statement = connection.createStatement();
+			int size = database.fetchTableSize(statement, entity.getTableName());
+			statement.close();
+			return size;
+		} catch (final SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (final SQLException e) {
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.wicket.markup.repeater.data.IDataProvider#iterator(int, int)
+	 */
+	@Override
+	public Iterator<? extends EntityInstance> iterator(int first, int count) {
+		Connection connection = null;
+		try {
+			final EntityDescriptor entity = getEntity();
+			final AbstractDatabaseDescriptor database = entity.getDatabase();
+			connection = database.createConnection();
+			final Statement statement = connection.createStatement();
+
+			// send the query to the database and analyze result meta-data
+			String query;
+			{
+				char b = database.getIdentifierBeginQuoteCharacter();
+				char e = database.getIdentifierEndQuoteCharacter();
+				String orderClause = database.getDefaultOrderClause();
+				query = "SELECT * FROM " + b + entity.getTableName() + e + orderClause + " LIMIT " + count + " OFFSET " + first;
+			}
+			final ResultSet resultSet = statement.executeQuery(query);
+			ResultSetReader reader = new ResultSetReader(resultSet, entity.getIdColumnName(), entity.getRawEntityListFieldOrder());
+			
+			// fetch data and fill the rows array
+			List<EntityInstance> rows = new ArrayList<EntityInstance>();
+			String[] fieldNames = EntityInstance.getFieldNames(entity, resultSet);
+			while (reader.next()) {
+				rows.add(new EntityInstance(entity, reader.getId(), fieldNames, reader.getRow()));
+			}
+			
+			// additional client-specific behavior
+			onResultAvailable(reader);
+
+			// clean up
+			resultSet.close();
+			statement.close();
+			return rows.iterator();
+
+		} catch (final SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (final SQLException e) {
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.wicket.markup.repeater.data.IDataProvider#model(java.lang.Object)
+	 */
+	@Override
+	public IModel<EntityInstance> model(EntityInstance object) {
+		return Model.of(object);
+	}
+	
+}
--- webapp.admin/src/name/martingeisse/admin/multi/RawGlobalEntityListPanel.java
+++ webapp.admin/src/name/martingeisse/admin/multi/RawGlobalEntityListPanel.java
@@ -6,17 +6,10 @@
 
 package name.martingeisse.admin.multi;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import name.martingeisse.admin.application.ApplicationConfiguration;
 import name.martingeisse.admin.readonly.IPropertyReadOnlyRenderer;
-import name.martingeisse.admin.schema.AbstractDatabaseDescriptor;
 import name.martingeisse.admin.schema.EntityDescriptor;
 import name.martingeisse.admin.single.EntityInstance;
 import name.martingeisse.admin.util.LinkUtil;
@@ -64,6 +57,15 @@ public class RawGlobalEntityListPanel extends Panel {
 	public int getWidth() {
 		// cannot return renderers.length, otherwise we depend on the order in which child components get their onBeforeRender() called
 		return getEntity().getRawEntityListFieldOrder().length;
+	}
+	
+	/**
+	 * Getter method for the entityDescriptorModel.
+	 * @return the entityDescriptorModel
+	 */
+	@SuppressWarnings("unchecked")
+	public IModel<EntityDescriptor> getEntityDescriptorModel() {
+		return (IModel<EntityDescriptor>)getDefaultModel();
 	}
 	
 	/**
@@ -119,106 +121,33 @@ public class RawGlobalEntityListPanel extends Panel {
 	/**
 	 * Custom {@link IDataProvider} implementation -- cannot be implemented by
 	 * {@link RawGlobalEntityListPanel} since that would cause an infinite
-	 * loop on detach().
+	 * loop on detach(). We also need the common logic from {@link EntityInstanceDataProvider}.
 	 */
-	private class MyDataProvider implements IDataProvider<EntityInstance> {
+	private class MyDataProvider extends EntityInstanceDataProvider {
 
-		/* (non-Javadoc)
-		 * @see org.apache.wicket.model.IDetachable#detach()
+		/**
+		 * Constructor.
 		 */
-		@Override
-		public void detach() {
+		public MyDataProvider() {
+			super(RawGlobalEntityListPanel.this.getEntityDescriptorModel());
 		}
 
 		/* (non-Javadoc)
-		 * @see org.apache.wicket.markup.repeater.data.IDataProvider#size()
+		 * @see name.martingeisse.admin.multi.EntityInstanceDataProvider#onResultAvailable(name.martingeisse.common.jdbc.ResultSetReader)
 		 */
 		@Override
-		public int size() {
-			Connection connection = null;
-			try {
-				final EntityDescriptor entity = (EntityDescriptor)getDefaultModelObject();
-				final AbstractDatabaseDescriptor database = entity.getDatabase();
-				connection = database.createConnection();
-				final Statement statement = connection.createStatement();
-				int size = database.fetchTableSize(statement, entity.getTableName());
-				statement.close();
-				return size;
-			} catch (final SQLException e) {
-				throw new RuntimeException(e);
-			} finally {
-				try {
-					if (connection != null) {
-						connection.close();
-					}
-				} catch (final SQLException e) {
+		protected void onResultAvailable(ResultSetReader reader) throws SQLException {
+
+			// determine the column names and renderers
+			int width = reader.getWidth();
+			renderers = new IPropertyReadOnlyRenderer[width];
+			for (int i=0; i<width; i++) {
+				renderers[i] = ApplicationConfiguration.getCapabilities().createPropertyReadOnlyRenderer(reader.getSqlFieldType(i));
+				if (renderers[i] == null) {
+					throw new RuntimeException("no renderer");
 				}
 			}
-		}
-
-		/* (non-Javadoc)
-		 * @see org.apache.wicket.markup.repeater.data.IDataProvider#iterator(int, int)
-		 */
-		@Override
-		public Iterator<? extends EntityInstance> iterator(int first, int count) {
-			Connection connection = null;
-			try {
-				final EntityDescriptor entity = (EntityDescriptor)getDefaultModelObject();
-				final AbstractDatabaseDescriptor database = entity.getDatabase();
-				connection = database.createConnection();
-				final Statement statement = connection.createStatement();
-
-				// send the query to the database and analyze result meta-data
-				String query;
-				{
-					char b = database.getIdentifierBeginQuoteCharacter();
-					char e = database.getIdentifierEndQuoteCharacter();
-					String orderClause = database.getDefaultOrderClause();
-					query = "SELECT * FROM " + b + entity.getTableName() + e + orderClause + " LIMIT " + count + " OFFSET " + first;
-				}
-				final ResultSet resultSet = statement.executeQuery(query);
-				ResultSetReader reader = new ResultSetReader(resultSet, entity.getIdColumnName(), entity.getRawEntityListFieldOrder());
-				int width = reader.getWidth();
-				
-				// fetch data and fill the rows array
-				List<EntityInstance> rows = new ArrayList<EntityInstance>();
-				String[] fieldNames = EntityInstance.getFieldNames(entity, resultSet);
-				while (reader.next()) {
-					rows.add(new EntityInstance(entity, reader.getId(), fieldNames, reader.getRow()));
-				}
-				
-				// determine the column names and renderers
-				renderers = new IPropertyReadOnlyRenderer[width];
-				for (int i=0; i<width; i++) {
-					renderers[i] = ApplicationConfiguration.getCapabilities().createPropertyReadOnlyRenderer(reader.getSqlFieldType(i));
-					if (renderers[i] == null) {
-						throw new RuntimeException("no renderer");
-					}
-				}
-
-				// clean up
-				resultSet.close();
-				statement.close();
-				return rows.iterator();
-
-			} catch (final SQLException e) {
-				throw new RuntimeException(e);
-			} finally {
-				try {
-					if (connection != null) {
-						connection.close();
-					}
-				} catch (final SQLException e) {
-				}
-			}
-		}
-
-		/* (non-Javadoc)
-		 * @see org.apache.wicket.markup.repeater.data.IDataProvider#model(java.lang.Object)
-		 */
-		@Override
-		public IModel<EntityInstance> model(EntityInstance object) {
-			return Model.of(object);
+			
 		}
 		
 	}
