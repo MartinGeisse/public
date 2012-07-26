38c711b36b86d5ab85faa22f685f0ae9c92af987
Martin Geisse <martingeisse@googlemail.com>
2012-07-26 23:25:45 +0200

    ...

--- webapp.common/src/name/martingeisse/common/computation/operation/IPlainOperation.java
+++ webapp.common/src/name/martingeisse/common/computation/operation/IPlainOperation.java
@@ -6,10 +6,18 @@
 
 package name.martingeisse.common.computation.operation;
 
+import name.martingeisse.common.computation.operation.context.IOperationContext;
+
 /**
  * An operation that can be executed without any further information.
  * Any input values must be set using appropriate sub-interfaces
  * before execution. This interface does support a return value though.
+ * 
+ * The operation context can be used to provide the operation with
+ * resources it needs without passing them around all the time. This
+ * is especially useful with resources such as database connections
+ * which are cumbersome to pass through intermediate helper operations
+ * that handle batch processing, logging, exception handling etc.
  * 
  * @param <T> the result type
  */
@@ -17,8 +25,9 @@ public interface IPlainOperation<T> {
 
 	/**
 	 * Executes this operation.
+	 * @param context the context
 	 * @return the result
 	 */
-	public T execute();
+	public T execute(IOperationContext context);
 	
 }
