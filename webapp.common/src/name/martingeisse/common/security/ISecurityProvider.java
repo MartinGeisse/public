1b81d2823e8a1c395fe2e2bcc38da3e432346833
Martin Geisse <martingeisse@googlemail.com>
2012-11-01 22:08:49 +0100

    ...

---
+++ webapp.common/src/name/martingeisse/common/security/ISecurityProvider.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2010 Martin Geisse
+ *
+ * This file is distributed under the terms of the MIT license.
+ */
+
+package name.martingeisse.common.security;
+
+import name.martingeisse.common.security.authentication.IAdminAuthenticationStrategy;
+import name.martingeisse.common.security.authorization.IAdminAuthorizationStrategy;
+
+/**
+ * This interface hides implementation-specific glue code between
+ * the security subsystem and the application.
+ */
+public interface ISecurityProvider {
+
+	/**
+	 * Used by the security subsystem to obtain the authentication strategy.
+	 * @return the authentication strategy
+	 */
+	public IAdminAuthenticationStrategy getAuthenticationStrategy();
+
+	/**
+	 * Used by the security subsystem to obtain the authorization strategy.
+	 * @return the authorization strategy
+	 */
+	public IAdminAuthorizationStrategy getAuthorizationStrategy();
+	
+	/**
+	 * Stores the specified login data and performs application-specific "on login"
+	 * actions.
+	 * @param loginData the login data to store
+	 */
+	public void onLogin(LoginData loginData);
+	
+	/**
+	 * This method is called by the security subsystem to obtain the login data
+	 * associated with the calling thread.
+	 * @return the login data, or null if not logged in
+	 */
+	public LoginData getLoginData();
+	
+	/**
+	 * Deletes the stored login data associated with the calling thread and
+	 * performs application-specific "on logout" actions.
+	 */
+	public void onLogout();
+	
+}
---
+++ webapp.common/src/name/martingeisse/common/security/authentication/IUserProperties.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2011 Martin Geisse
+ *
+ * This file is distributed under the terms of the MIT license.
+ */
+
+package name.martingeisse.common.security.authentication;
+
+
+/**
+ * This interface is implemented by data objects that know some
+ * or all of a user's properties. Properties are simple key/value
+ * pairs with string keys and arbitrary values.
+ */
+public interface IUserProperties {
+
+	/**
+	 * Checks whether this property set is empty.
+	 * @return true if empty, false if nonempty
+	 */
+	public boolean isEmpty();
+
+	/**
+	 * @return the number of key/value pairs in this object
+	 */
+	public int size();
+
+	/**
+	 * Checks if the specified key is contained in this property set.
+	 * @param key the key
+	 * @return true if this object contains the key, false if not
+	 */
+	public boolean containsKey(String key);
+
+	/**
+	 * Returns an {@link Iterable} object for the keys contained in
+	 * this object.
+	 * @return the keys
+	 */
+	public Iterable<String> getKeys();
+
+	/**
+	 * Returns the value for the specified key.
+	 * @param key the key
+	 * @return the value
+	 */
+	public Object get(String key);
+
+}
