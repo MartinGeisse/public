29dfc5d28ab4575ff4730c0bda3f8904653821c6
Martin Geisse <geisse@Shopgates-Mac-mini-3.local>
2012-11-05 08:47:30 +0100

    ...

---
+++ webapp.common/src/name/martingeisse/common/cache/group/RowCacheGroup.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) 2012 Shopgate GmbH
+ */
+
+package name.martingeisse.common.cache.group;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import name.martingeisse.common.cache.querydsl.RowLoader;
+import name.martingeisse.common.computation.mapping.IMapping;
+import name.martingeisse.common.util.Wrapper;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.LoadingCache;
+import com.mysema.query.sql.RelationalPath;
+import com.mysema.query.types.Expression;
+import com.mysema.query.types.Predicate;
+
+/**
+ * Represents a group of caches using {@link RowLoader}s. These caches store the
+ * same values using different keys. Whenever a value is stored in one of the caches,
+ * it is also stored in the other caches. They are not guaranteed to *contain* the
+ * same values since they may evict values independently.
+ */
+public class RowCacheGroup<V> {
+
+	/**
+	 * the caches
+	 */
+	private final List<LoadingCache<?, Wrapper<V>>> caches = new ArrayList<LoadingCache<?, Wrapper<V>>>();
+
+	/**
+	 * the cacheBuilder
+	 */
+	private final CacheBuilder<?, ?> cacheBuilder;
+
+	/**
+	 * the path
+	 */
+	private final RelationalPath<V> path;
+
+	/**
+	 * the additionalPredicates
+	 */
+	private final Predicate[] additionalPredicates;
+	
+	/**
+	 * the loaderToCache
+	 */
+	private final Map<RowLoader<?, V>, LoadingCache<?, Wrapper<V>>> loaderToCache = new HashMap<RowLoader<?, V>, LoadingCache<?, Wrapper<V>>>();
+	
+	/**
+	 * the cacheToValueToKeyMapping
+	 */
+	private final Map<LoadingCache<?, Wrapper<V>>, IMapping<V, ?>> cacheToValueToKeyMapping = new HashMap<LoadingCache<?, Wrapper<V>>, IMapping<V,?>>();
+
+	/**
+	 * Constructor.
+	 * @param cacheBuilder the cache builder to use
+	 * @param path the table path
+	 * @param additionalPredicates additional predicates (if any)
+	 */
+	public RowCacheGroup(CacheBuilder<?, ?> cacheBuilder, RelationalPath<V> path, Predicate... additionalPredicates) {
+		this.cacheBuilder = cacheBuilder;
+		this.path = path;
+		this.additionalPredicates = additionalPredicates;
+	}
+
+	/**
+	 * Distributes the specified value to all caches. This adds the value to all
+	 * caches except the origin, because the origin will store the value itself.
+	 * @param wrapper the wrapper to distribute
+	 * @param origin the origin of the value
+	 */
+	private void distribute(Wrapper<V> wrapper, LoadingCache<?, ?> origin) {
+		for (LoadingCache<?, Wrapper<V>> cache : caches) {
+			if (cache != origin) {
+				IMapping<?, ?> valueToKeyMapping = cacheToValueToKeyMapping.get(cache);
+				distributeHelper(wrapper, valueToKeyMapping, cache);
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	@SuppressWarnings({"unchecked", "rawtypes"})
+	private void distributeHelper(Wrapper wrapper, IMapping valueToKeyMapping, LoadingCache cache) {
+		Object key = valueToKeyMapping.map(wrapper.getValue());
+		cache.put(key, wrapper);
+	}
+
+	/**
+	 * Creates a new cache using the specified key expression to fetch missing values, and the
+	 * valueToKeyMapping to generate keys for the cache.
+	 * 
+	 * @param keyExpression the key expression
+	 * @param valueToKeyMapping the mapping that generates keys for the cache
+	 * @return the cache
+	 */
+	public <K> LoadingCache<K, Wrapper<V>> addCache(Expression<?> keyExpression, IMapping<V, K> valueToKeyMapping) {
+
+		// create the loader
+		RowLoader<K, V> rowLoader = new RowLoader<K, V>(path, keyExpression, additionalPredicates) {
+			@Override
+			protected Wrapper<V> transformValue(K key, V row) {
+				Wrapper<V> wrapper = super.transformValue(key, row);
+				if (wrapper.getValue() != null) {
+					distribute(wrapper, loaderToCache.get(this));
+				}
+				return wrapper;
+			}
+		};
+
+		// create the cache
+		@SuppressWarnings("unchecked")
+		CacheBuilder<K, Wrapper<V>> typedCacheBuilder = (CacheBuilder<K, Wrapper<V>>)cacheBuilder;
+		LoadingCache<K, Wrapper<V>> cache = typedCacheBuilder.build(rowLoader);
+		caches.add(cache);
+		loaderToCache.put(rowLoader, cache);
+		cacheToValueToKeyMapping.put(cache, valueToKeyMapping);
+		return cache;
+
+	}
+
+}
