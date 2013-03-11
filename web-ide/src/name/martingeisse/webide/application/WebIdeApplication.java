1a2921f74d915cd7d5579d8b0707498d3b8e8f4c
Martin Geisse <martingeisse@googlemail.com>
2013-03-11 22:19:03 +0100

    ...

--- web-ide/src/name/martingeisse/webide/application/WebIdeApplication.java
+++ web-ide/src/name/martingeisse/webide/application/WebIdeApplication.java
@@ -6,8 +6,6 @@
 
 package name.martingeisse.webide.application;
 
-import javax.servlet.http.HttpServletResponse;
-
 import name.martingeisse.common.util.TemporaryFolder;
 import name.martingeisse.webide.workbench.WorkbenchPage;
 
@@ -15,9 +13,6 @@ import org.apache.wicket.Page;
 import org.apache.wicket.atmosphere.EventBus;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.request.resource.ResourceReferenceRegistry;
-import org.atmosphere.cpr.AsyncIOWriter;
-import org.atmosphere.cpr.AtmosphereRequest;
-import org.atmosphere.cpr.AtmosphereResponse;
 
 /**
  * The wicket application.
@@ -25,20 +20,9 @@ import org.atmosphere.cpr.AtmosphereResponse;
 public class WebIdeApplication extends WebApplication {
 	
 	/**
-	 * the crippledAtmosphereHttpServletResponse
-	 */
-	private static final HttpServletResponse crippledAtmosphereHttpServletResponse;
-
-	/**
 	 * the instance
 	 */
 	private volatile static WebIdeApplication instance;
-
-	// find the crippled response object which Atmosphere is using so we can ignore it later
-	static {
-		AtmosphereResponse dummyResponse = new AtmosphereResponse((AsyncIOWriter)null, (AtmosphereRequest)null, false);
-		crippledAtmosphereHttpServletResponse = (HttpServletResponse)dummyResponse.getResponse();
-	}
 	
 	/**
 	 * This method allows the caller to obtain the application
@@ -59,23 +43,6 @@ public class WebIdeApplication extends WebApplication {
 		new EventBus(this);
 		TemporaryFolder.initialize("web-ide");
 		setHeaderResponseDecorator(new MyHeaderResponseDecorator());
-//		setRequestCycleProvider(new IRequestCycleProvider() {
-//			@Override
-//			public RequestCycle get(RequestCycleContext context) {
-//				if (context.getResponse() instanceof WebResponse) {
-//					WebResponse webResponse = (WebResponse)context.getResponse();
-//					HttpServletResponse httpServletResponse = (HttpServletResponse)webResponse.getContainerResponse();
-//					if (httpServletResponse instanceof AtmosphereResponse) {
-//						AtmosphereResponse atmosphereResponse = (AtmosphereResponse)httpServletResponse;
-//						ServletResponse atmosphereInternalResponse = atmosphereResponse.getResponse();
-//						if (atmosphereInternalResponse == crippledAtmosphereHttpServletResponse) {
-//							System.err.println("--- found crippled response object ---");
-//						}
-//					}
-//				}
-//				return new RequestCycle(context);
-//			}
-//		});
 	}
 
 	/* (non-Javadoc)
