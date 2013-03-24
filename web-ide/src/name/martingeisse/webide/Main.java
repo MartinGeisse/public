bcf948173391a3e00c0b5c33e111620a5d26d72d
Martin Geisse <martingeisse@googlemail.com>
2013-03-24 22:25:14 +0100

    ...

--- web-ide/src/name/martingeisse/webide/Main.java
+++ web-ide/src/name/martingeisse/webide/Main.java
@@ -13,12 +13,12 @@ import java.io.IOException;
 
 import javax.servlet.Servlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 import name.martingeisse.webide.application.IdeLauncher;
 import name.martingeisse.webide.application.WebIdeApplication;
 import name.martingeisse.webide.editor.codemirror.ot.CodeMirrorOtServer;
-import name.martingeisse.webide.editor.codemirror.ot.HelloServer;
 import name.martingeisse.webide.ssh.ShellFactory;
 
 import org.apache.sshd.SshServer;
@@ -29,9 +29,11 @@ import org.apache.wicket.protocol.http.WicketFilter;
 import org.atmosphere.cpr.MeteorServlet;
 import org.eclipse.jetty.rewrite.handler.ProxyRule;
 import org.eclipse.jetty.rewrite.handler.RewriteHandler;
+import org.eclipse.jetty.rewrite.handler.RewritePatternRule;
 import org.eclipse.jetty.rewrite.handler.Rule;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -69,23 +71,23 @@ public class Main {
 		sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider("hostkey.ser"));
 		sshd.setShellFactory(new ShellFactory());
 		sshd.start();
-		
+
 	}
-	
+
 	/**
 	 * Launches the web user interface.
 	 */
 	private static void launchWeb() throws Exception {
-		
+
 		// create and configure a servlet context
 		final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 		context.setContextPath("/");
 		context.getSessionHandler().getSessionManager().setMaxInactiveInterval(30 * 60);
 		context.getSessionHandler().getSessionManager().setSessionIdPathParameterName(null);
-		
+
 		// add the Meteor servlet, also adding Wicket filter configuration
 		final Servlet atmosphereServlet = new MeteorServlet();
-		ServletHolder atmosphereServletHolder = new ServletHolder(atmosphereServlet);
+		final ServletHolder atmosphereServletHolder = new ServletHolder(atmosphereServlet);
 		atmosphereServletHolder.setInitParameter(FILTER_CLASS, WicketFilter.class.getCanonicalName());
 		atmosphereServletHolder.setInitParameter(SERVLET_CLASS, DefaultServlet.class.getCanonicalName());
 		atmosphereServletHolder.setInitParameter("org.atmosphere.useWebSocket", "false");
@@ -93,31 +95,28 @@ public class Main {
 		atmosphereServletHolder.setInitParameter("applicationClassName", WebIdeApplication.class.getCanonicalName());
 		atmosphereServletHolder.setInitParameter(WicketFilter.FILTER_MAPPING_PARAM, "/*");
 		atmosphereServletHolder.setInitParameter("org.atmosphere.cpr.sessionSupport", "true");
-//		atmosphereServletHolder.setInitParameter("org.atmosphere.websocket.WebSocketProtocol", "org.atmosphere.websocket.protocol.EchoProtocol");
+		//		atmosphereServletHolder.setInitParameter("org.atmosphere.websocket.WebSocketProtocol", "org.atmosphere.websocket.protocol.EchoProtocol");
 		atmosphereServletHolder.setInitParameter("org.atmosphere.cpr.broadcastFilterClasses", "org.apache.wicket.atmosphere.TrackMessageSizeFilter");
 		// atmosphereServletHolder.setAsyncSupported(true);
 		context.addServlet(atmosphereServletHolder, "/*");
 
 		// create a multiplexing handler for Node.js servers, and start those servers
-		new HelloServer().start();
 		new CodeMirrorOtServer().start();
-		ProxyRule helloProxyRule = new ProxyRule();
-		helloProxyRule.setPattern("/internal-api/ot/*");
-		helloProxyRule.setProxyTo("http://localhost:8081");
-		
-		RewriteHandler rewriteHandler = new RewriteHandler();
+		final OtPathRule otPathRule = new OtPathRule();
+
+		final RewriteHandler rewriteHandler = new RewriteHandler();
 		rewriteHandler.addRule(new DummyRule());
-		rewriteHandler.addRule(helloProxyRule);
-		HandlerCollection handlerCollection = new HandlerCollection();
+		rewriteHandler.addRule(otPathRule);
+		final HandlerCollection handlerCollection = new HandlerList();
 		handlerCollection.addHandler(rewriteHandler);
 		handlerCollection.addHandler(context);
-		
+
 		// start the server
 		final Server server = new Server(8080);
-		server.setHandler(handlerCollection);
+		server.setHandler(context);
 		server.start();
 		server.join();
-		
+
 	}
 
 	private static class DummyRule extends Rule {
@@ -126,10 +125,58 @@ public class Main {
 		 * @see org.eclipse.jetty.rewrite.handler.Rule#matchAndApply(java.lang.String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 		 */
 		@Override
-		public String matchAndApply(String target, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		public String matchAndApply(final String target, final HttpServletRequest request, final HttpServletResponse response) throws IOException {
 			return null;
 		}
-		
+
 	}
-	
+
+	private static class OtPathRule extends Rule {
+
+		/**
+		 * the rewriteRule
+		 */
+		private final RewritePatternRule rewriteRule;
+
+		/**
+		 * the proxyRule
+		 */
+		private final ProxyRule proxyRule;
+
+		/**
+		 * Constructor.
+		 */
+		public OtPathRule() {
+			rewriteRule = new RewritePatternRule();
+			rewriteRule.setPattern("/internal-api/ot/*");
+			rewriteRule.setReplacement("/");
+			proxyRule = new ProxyRule();
+			proxyRule.setPattern("/*");
+			proxyRule.setProxyTo("http://localhost:8081");
+		}
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.jetty.rewrite.handler.Rule#matchAndApply(java.lang.String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+		 */
+		@Override
+		public String matchAndApply(final String target, final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+			if (target.startsWith("/internal-api/ot/")) {
+				final String translatedTarget = rewriteRule.apply(target, request, response);
+				final HttpServletRequestWrapper requestWrapper = new HttpServletRequestWrapper(request) {
+					@Override
+					public String getRequestURI() {
+						return translatedTarget;
+					}
+				};
+				return proxyRule.matchAndApply(target, requestWrapper, response);
+			} else if (target.startsWith("/socket.io/")) {
+				System.err.append("direct proxy for: " + target);
+				return proxyRule.matchAndApply(target, request, response);
+			} else {
+				return null;
+			}
+		}
+
+	}
+
 }
