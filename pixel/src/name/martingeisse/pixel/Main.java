9d22d4c80d33665770c3ee43e97f9da4fde00fb8
Martin Geisse <martingeisse@googlemail.com>
2013-10-24 23:32:21 +0200

    ...

--- pixel/src/name/martingeisse/pixel/Main.java
+++ pixel/src/name/martingeisse/pixel/Main.java
@@ -11,10 +11,8 @@ import javax.imageio.ImageIO;
 
 import name.martingeisse.pixel.common.Picture;
 import name.martingeisse.pixel.nonogram.NonogramBoard;
-import name.martingeisse.pixel.nonogram.common.NonogramSolutionStrategy;
 import name.martingeisse.pixel.nonogram.fast.NonogramFastStrategy;
 import name.martingeisse.pixel.nonogram.misc.ForkStrategy;
-import name.martingeisse.pixel.nonogram.misc.ForkingStrategyBundle;
 import name.martingeisse.pixel.nonogram.misc.NonogramStarterStrategy;
 import name.martingeisse.pixel.nonogram.misc.SliceSpanCombinationsStrategy;
 
@@ -30,37 +28,17 @@ public class Main {
 	 */
 	public static void main(String[] args) throws Exception {
 	
-		BufferedImage inputImage = ImageIO.read(new File("testpic1.png"));
+		BufferedImage inputImage = ImageIO.read(new File("images/edited/wacken.png"));
 		Picture picture = new Picture(inputImage);
 		NonogramBoard board = new NonogramBoard(picture, false);
 		
-		board.setPixel(47, 25, true);
 		new NonogramStarterStrategy().run(board);
-		for (int j=0; j<500; j++) {
-			new NonogramFastStrategy().run(board);
+		 for (int j=0; j<500; j++) {
+			 new NonogramFastStrategy().run(board);
 			new SliceSpanCombinationsStrategy().run(board);
 		}
+		 /*
 		System.out.println("fork 1");
-//		new ForkStrategy(1).run(board);
-//		for (int j=0; j<500; j++) {
-//			new NonogramFastStrategy().run(board);
-//			new SliceSpanCombinationsStrategy().run(board);
-//		}
-		NonogramSolutionStrategy.forkOn(board, 11, 2, 1);
-		NonogramSolutionStrategy.forkOn(board, 27, 24, 1);
-		NonogramSolutionStrategy.forkOn(board, 29, 19, 1);
-		NonogramSolutionStrategy.forkOn(board, 35, 20, 1);
-		for (int j=0; j<500; j++) {
-			new NonogramFastStrategy().run(board);
-			new SliceSpanCombinationsStrategy().run(board);
-		}
-
-		// NonogramSolutionStrategy.forkOn(board, 15, 23, 3);
-//		+++ 15, 23 (1)
-//		+++ 38, 18 (1)
-//		+++ 38, 19 (1)
-//		+++ 38, 20 (1)
-		System.out.println("fork 1.5");
 		new ForkStrategy(1).run(board);
 		for (int j=0; j<500; j++) {
 			new NonogramFastStrategy().run(board);
@@ -76,10 +54,11 @@ public class Main {
 			new NonogramFastStrategy().run(board);
 			new SliceSpanCombinationsStrategy().run(board);
 		}
+		*/
 
 		
 		
-		System.out.println("fork 2");
+		// System.out.println("fork 2");
 		// new ForkStrategy(2).run(board);
 		
 		board.renderToPngFile(20, true, new File("test.png"));
