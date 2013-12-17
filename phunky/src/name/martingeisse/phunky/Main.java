7631a3355727b5ca98b36caad3cae3b247f134ab
Martin Geisse <martingeisse@googlemail.com>
2013-12-17 07:01:54 +0100

    ...

---
+++ phunky/src/name/martingeisse/phunky/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2010 Martin Geisse
+ *
+ * This file is distributed under the terms of the MIT license.
+ */
+
+package name.martingeisse.phunky;
+
+/**
+ * Test main class.
+ */
+public class Main {
+
+	/**
+	 * The main method.
+	 * @param args command-line arguments
+	 */
+	public static void main(String[] args) {
+	}
+	
+}
