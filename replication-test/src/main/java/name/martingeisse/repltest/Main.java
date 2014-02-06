b565fc01105f4e206741b005caa46c74b2fb8809
Martin Geisse <martingeisse@googlemail.com>
2014-02-06 19:56:07 +0100

    ...

--- replication-test/src/main/java/name/martingeisse/repltest/Main.java
+++ replication-test/src/main/java/name/martingeisse/repltest/Main.java
@@ -6,9 +6,17 @@
 
 package name.martingeisse.repltest;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 import com.github.shyiko.mysql.binlog.BinaryLogClient;
 import com.github.shyiko.mysql.binlog.BinaryLogClient.EventListener;
+import com.github.shyiko.mysql.binlog.event.DeleteRowsEventData;
 import com.github.shyiko.mysql.binlog.event.Event;
+import com.github.shyiko.mysql.binlog.event.EventData;
+import com.github.shyiko.mysql.binlog.event.TableMapEventData;
+import com.github.shyiko.mysql.binlog.event.UpdateRowsEventData;
+import com.github.shyiko.mysql.binlog.event.WriteRowsEventData;
 
 /**
  * Hello world!
@@ -16,16 +24,75 @@ import com.github.shyiko.mysql.binlog.event.Event;
 public class Main {
 
 	/**
+	 * the tableMap
+	 */
+	private static Map<Long, String> tableNames = new HashMap<Long, String>();
+	
+	/**
 	 * The main method.
 	 * @param args ignored
 	 * @throws Exception on errors
 	 */
 	public static void main(final String[] args) throws Exception {
+		
+		// TODO use DESCRIBE TABLE to map column IDs to column names and
+		// especially to find which one is the ID
+		
 		final BinaryLogClient client = new BinaryLogClient("localhost", 3306, "ucademy", "username", "password");
 		client.registerEventListener(new EventListener() {
 			@Override
 			public void onEvent(final Event event) {
-				System.out.println("event: " + event);
+				EventData untypedEventData = event.getData();
+				switch (event.getHeader().getEventType()) {
+				
+				case TABLE_MAP: {
+					TableMapEventData data = (TableMapEventData)untypedEventData;
+					if (data.getDatabase().equals("ucademy")) {
+						tableNames.put(data.getTableId(), data.getTable());
+					}
+					break;
+				}
+					
+				case WRITE_ROWS: {
+					WriteRowsEventData data = (WriteRowsEventData)untypedEventData;
+					long tableId = data.getTableId();
+					String tableName = tableNames.get(tableId);
+					for (Serializable[] row : data.getRows()) {
+						long id = Long.parseLong(row[0].toString());
+						System.out.println("insert " + tableName + "." + id);
+					}
+					break;
+				}
+					
+				case UPDATE_ROWS: {
+					UpdateRowsEventData data = (UpdateRowsEventData)untypedEventData;
+					long tableId = data.getTableId();
+					String tableName = tableNames.get(tableId);
+					for (Map.Entry<Serializable[], Serializable[]> entry : data.getRows()) {
+						Serializable[] oldRow = entry.getKey();
+						long id = Long.parseLong(oldRow[0].toString());
+						// Serializable[] newRow = entry.getValue();
+						System.out.println("update: " + tableName + "." + id);
+					}
+					break;
+				}
+					
+				case DELETE_ROWS: {
+					DeleteRowsEventData data = (DeleteRowsEventData)untypedEventData;
+					long tableId = data.getTableId();
+					String tableName = tableNames.get(tableId);
+					for (Serializable[] row : data.getRows()) {
+						long id = Long.parseLong(row[0].toString());
+						System.out.println("delete " + tableName + "." + id);
+					}
+					break;
+				}
+					
+				default: {
+					break;
+				}
+					
+				}
 			}
 		});
 		client.connect();
