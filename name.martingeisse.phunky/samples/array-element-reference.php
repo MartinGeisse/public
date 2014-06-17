88fe4fc64cc7c1c96578627bca5de430edf637ba
Martin Geisse <martin.geisse@shopgate.com>
2014-06-17 17:05:31 +0200

    ...

---
+++ name.martingeisse.phunky/samples/array-element-reference.php
@@ -0,0 +1,47 @@
+<?php
+
+// represents an instance variable of an object
+$objectVariable = null;
+
+// represents a method of that object
+function firstMethod($a) {
+	global $objectVariable;
+	$objectVariable = $a;
+}
+
+// represents another method of that object
+function secondMethod($a) {
+	global $objectVariable;
+	var_dump($objectVariable);
+}
+
+// ------------------------------
+// main code
+// ------------------------------
+
+// load model
+$data = array(
+	'Shop' => array(
+		'id' => 1,
+		'name' => 'testshop',
+	),
+	'User' => array(
+		'id' => 42,
+		'name' => 'testuser',
+	),
+);
+
+// manipulate data (sometimes easier using references)
+$ref =& $data['Shop'];
+$ref['name'] = 'liveshop';
+unset($ref);
+
+// store in object
+firstMethod($data);
+
+// do something else
+$data['Shop']['name'] = 'foobar';
+
+// use the object again
+secondMethod();
+
