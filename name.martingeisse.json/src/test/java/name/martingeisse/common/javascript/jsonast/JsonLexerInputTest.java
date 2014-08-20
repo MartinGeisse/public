44758a9f32deb2eff9b2339f352fe542d54ec1f3
Martin Geisse <martingeisse@googlemail.com>
2014-08-20 20:38:15 +0200

    ...

---
+++ name.martingeisse.json/src/test/java/name/martingeisse/common/javascript/jsonast/JsonLexerInputTest.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2010 Martin Geisse
+ *
+ * This file is distributed under the terms of the MIT license.
+ */
+
+package name.martingeisse.common.javascript.jsonast;
+
+/**
+ * 
+ */
+public class JsonLexerInputTest {
+
+	/**
+	 * 
+	 */
+	@test
+	public void testInitialState() {
+		
+	}
+	
+}
