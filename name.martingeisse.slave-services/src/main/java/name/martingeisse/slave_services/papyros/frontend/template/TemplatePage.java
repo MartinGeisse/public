f68d1ef22016226db99910554a53bd2585e6ff34
Martin Geisse <martin.geisse@shopgate.com>
2014-09-26 13:34:12 +0200

    ...

--- name.martingeisse.slave-services/src/main/java/name/martingeisse/slave_services/papyros/frontend/template/TemplatePage.java
+++ name.martingeisse.slave-services/src/main/java/name/martingeisse/slave_services/papyros/frontend/template/TemplatePage.java
@@ -4,39 +4,21 @@
 
 package name.martingeisse.slave_services.papyros.frontend.template;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import name.martingeisse.common.terms.IConsumer;
 import name.martingeisse.slave_services.common.frontend.AbstractFrontendPage;
-import name.martingeisse.slave_services.common.frontend.components.CompilerMarkerListPanel;
-import name.martingeisse.slave_services.common.frontend.components.Iframe;
 import name.martingeisse.slave_services.common.frontend.components.PageParameterDrivenTabPanel;
-import name.martingeisse.slave_services.entity.QTemplate;
 import name.martingeisse.slave_services.entity.Template;
 import name.martingeisse.slave_services.entity.TemplateFamily;
 import name.martingeisse.slave_services.entity.TemplatePreviewDataSet;
 import name.martingeisse.slave_services.papyros.backend.PapyrosDataUtil;
 import name.martingeisse.slave_services.papyros.frontend.components.PreviewTemplateIframe;
 import name.martingeisse.slave_services.papyros.frontend.family.TemplateFamilyPage;
-import name.martingeisse.sql.EntityConnectionManager;
-import name.martingeisse.wicket.component.codemirror.CodeMirrorBehavior;
-import name.martingeisse.wicket.component.codemirror.compile.CodeMirrorAutocompileBehavior;
 import name.martingeisse.wicket.component.codemirror.compile.CompilerMarker;
-import name.martingeisse.wicket.component.codemirror.compile.CompilerMarkerErrorLevelComparator;
-import name.martingeisse.wicket.component.codemirror.compile.CompilerResult;
-import name.martingeisse.wicket.component.codemirror.compile.ICompiler;
-import name.martingeisse.wicket.component.codemirror.modes.CodeMirrorModes;
-import name.martingeisse.wicket.util.AjaxRequestUtil;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.wicket.Component;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.markup.html.panel.Fragment;
@@ -44,13 +26,8 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import com.mysema.query.sql.dml.SQLUpdateClause;
-
 /**
  * The main page to manage a template.
- * 
- * TODO edit ohne save, dann reload -- editor-inhalt enthält noch das original, aber preview die editierte version.
- * sollte einheitlich sein.
  */
 public class TemplatePage extends AbstractFrontendPage {
 
@@ -97,40 +74,12 @@ public class TemplatePage extends AbstractFrontendPage {
 
 				} else if (selector.equals(".edit")) {
 
-					//
-					final TemplateAutocompiler templateAutocompiler = new TemplateAutocompiler();
-					previewDataSet = PapyrosDataUtil.loadFirstPreviewDataSet(template.getTemplateFamilyId());
-					setCompilerMarkersFromResult(ICompiler.Util.compileSafe(templateAutocompiler, editableContent));
-					
-					final Fragment fragment = new Fragment(id, "tabEdit", TemplatePage.this);
-					
-					// build the edit form and CodeMirror
-					final Form<Void> form = new Form<Void>("form") {
+					return new EditTemplateFragment(id, "tabEdit", TemplatePage.this, family, template) {
 						@Override
 						protected void onSubmit() {
-							final QTemplate q = QTemplate.template;
-							final SQLUpdateClause update = EntityConnectionManager.getConnection().createUpdate(q);
-							update.set(q.content, editableContent).where(q.id.eq(template.getId()));
-							update.execute();
 							setResponsePage(TemplatePage.class, createTabLinkPageParameters(".preview"));
 						}
 					};
-					final CodeMirrorBehavior codeMirrorBehavior = new CodeMirrorBehavior(CodeMirrorModes.JAVASCRIPT);
-					final CodeMirrorAutocompileBehavior autocompileBehavior = new CodeMirrorAutocompileBehavior(templateAutocompiler);
-					form.add(new TextArea<>("textarea", new PropertyModel<>(TemplatePage.this, "content")).add(codeMirrorBehavior).add(autocompileBehavior));
-					fragment.add(form);
-
-					// build the preview
-					Iframe previewIframe = new PreviewTemplateIframe("previewIframe", new PropertyModel<String>(TemplatePage.this, "editableContent"), Model.of(previewDataSet));
-					previewIframe.setOutputMarkupId(true);
-					fragment.add(previewIframe);
-					
-					// build the markers list
-					CompilerMarkerListPanel compilerMarkersListPanel = new CompilerMarkerListPanel("compilerMarkers", new PropertyModel<List<CompilerMarker>>(TemplatePage.this, "compilerMarkers"));
-					fragment.add(compilerMarkersListPanel);
-					autocompileBehavior.setResultConsumer(new CompilerResultConsumer(previewIframe, compilerMarkersListPanel));
-					
-					return fragment;
 
 				} else {
 					return new EmptyPanel(id);
@@ -144,19 +93,35 @@ public class TemplatePage extends AbstractFrontendPage {
 	}
 
 	/**
-	 * Getter method for the content.
-	 * @return the content
+	 * Getter method for the editableContent.
+	 * @return the editableContent
 	 */
-	public String getContent() {
+	public String getEditableContent() {
 		return editableContent;
 	}
 
 	/**
-	 * Setter method for the content.
-	 * @param content the content to set
+	 * Setter method for the editableContent.
+	 * @param editableContent the editableContent to set
 	 */
-	public void setContent(final String content) {
-		this.editableContent = content;
+	public void setEditableContent(String editableContent) {
+		this.editableContent = editableContent;
+	}
+
+	/**
+	 * Getter method for the previewDataSet.
+	 * @return the previewDataSet
+	 */
+	public TemplatePreviewDataSet getPreviewDataSet() {
+		return previewDataSet;
+	}
+
+	/**
+	 * Setter method for the previewDataSet.
+	 * @param previewDataSet the previewDataSet to set
+	 */
+	public void setPreviewDataSet(TemplatePreviewDataSet previewDataSet) {
+		this.previewDataSet = previewDataSet;
 	}
 
 	/**
@@ -166,51 +131,13 @@ public class TemplatePage extends AbstractFrontendPage {
 	public List<CompilerMarker> getCompilerMarkers() {
 		return compilerMarkers;
 	}
-	
-	/**
-	 * 
-	 */
-	private void setCompilerMarkersFromResult(final CompilerResult compilerResult) {
-		compilerMarkers = new ArrayList<>(compilerResult.getMarkers());
-		Collections.sort(compilerMarkers, new CompilerMarkerErrorLevelComparator());
-	}
 
 	/**
-	 * Consumes the compiler result after auto-compilation, in addition to the
-	 * CodeMirror component adding markers in the editor automatically.
+	 * Setter method for the compilerMarkers.
+	 * @param compilerMarkers the compilerMarkers to set
 	 */
-	private class CompilerResultConsumer implements IConsumer<CompilerResult>, Serializable {
-
-		/**
-		 * the previewIframe
-		 */
-		private final Iframe previewIframe;
-		
-		/**
-		 * the compilerMarkersContainer
-		 */
-		private final WebMarkupContainer compilerMarkersContainer;
-
-		/**
-		 * Constructor.
-		 * @param previewIframe the iframe used for preview
-		 * @param compilerMarkersContainer the container for the markers list
-		 */
-		public CompilerResultConsumer(final Iframe previewIframe, final WebMarkupContainer compilerMarkersContainer) {
-			this.previewIframe = previewIframe;
-			this.compilerMarkersContainer = compilerMarkersContainer;
-		}
-
-		/* (non-Javadoc)
-		 * @see name.martingeisse.common.terms.IConsumer#consume(java.lang.Object)
-		 */
-		@Override
-		public void consume(final CompilerResult compilerResult) {
-			setCompilerMarkersFromResult(compilerResult);
-			AjaxRequestUtil.markForRender(compilerMarkersContainer);
-			editableContent = compilerResult.getDocument();
-			previewIframe.renderReloadScript();
-		}
-
+	public void setCompilerMarkers(List<CompilerMarker> compilerMarkers) {
+		this.compilerMarkers = compilerMarkers;
 	}
+
 }
