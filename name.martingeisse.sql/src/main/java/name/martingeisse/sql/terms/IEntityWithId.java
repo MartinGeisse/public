0a89e7aa2e5492817fbf51a7980d0e1a4ac8e4f3
Martin Geisse <martin.geisse@shopgate.com>
2014-11-30 10:11:18 +0100

    ...

--- name.martingeisse.sql/src/main/java/name/martingeisse/sql/terms/IEntityWithId.java
+++ name.martingeisse.sql/src/main/java/name/martingeisse/sql/terms/IEntityWithId.java
@@ -5,6 +5,10 @@
 package name.martingeisse.sql.terms;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import name.martingeisse.common.terms.IGetAndSetId;
 
 /**
@@ -13,4 +17,33 @@ import name.martingeisse.common.terms.IGetAndSetId;
  * @param <ID> the ID type
  */
 public interface IEntityWithId<ID> extends IGetAndSetId<ID>, Serializable {
+
+	/**
+	 * Contains utility methods related to {@link IEntityWithId}.
+	 */
+	public static class Util {
+
+		/**
+		 * Prevent instantiation.
+		 */
+		private Util() {
+		}
+		
+		/**
+		 * Extracts the IDs of a collection of entities. The IDs are returned
+		 * in iteration order of the collection.
+		 * 
+		 * @param entities the entities
+		 * @return the IDs
+		 */
+		public static <ID> List<ID> extractIds(Collection<? extends IEntityWithId<ID>> entities) {
+			List<ID> result = new ArrayList<ID>();
+			for (IEntityWithId<ID> entity : entities) {
+				result.add(entity.getId());
+			}
+			return result;
+		}
+
+	}
+
 }
