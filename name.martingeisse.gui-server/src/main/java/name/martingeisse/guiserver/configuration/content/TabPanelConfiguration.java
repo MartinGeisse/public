0d6e978eb4813be52e2fc0b760b4bbfcdb1296d5
Martin Geisse <martin.geisse@shopgate.com>
2015-02-09 07:58:13 +0100

    ...

--- name.martingeisse.gui-server/src/main/java/name/martingeisse/guiserver/configuration/content/TabPanelConfiguration.java
+++ name.martingeisse.gui-server/src/main/java/name/martingeisse/guiserver/configuration/content/TabPanelConfiguration.java
@@ -7,6 +7,7 @@ package name.martingeisse.guiserver.configuration.content;
 import java.util.ArrayList;
 import java.util.List;
 
+import name.martingeisse.guiserver.configuration.Configuration;
 import name.martingeisse.wicket.component.misc.PageParameterDrivenTabPanel;
 import name.martingeisse.wicket.component.misc.PageParameterDrivenTabPanel.AbstractTabInfo;
 
@@ -23,18 +24,23 @@ import com.google.common.collect.ImmutableList;
 /**
  * Represents a tab panel.
  */
-public final class TabPanelConfiguration extends AbstractComponentConfiguration {
+public final class TabPanelConfiguration extends AbstractComponentConfiguration implements IConfigurationSnippet {
 
 	/**
 	 * the parameterName
 	 */
 	private final String parameterName;
-	
+
 	/**
 	 * the tabs
 	 */
 	private final List<TabEntry> tabs;
 
+	/**
+	 * the snippetHandle
+	 */
+	private int snippetHandle;
+	
 	/**
 	 * Constructor.
 	 * @param id the wicket id
@@ -46,6 +52,22 @@ public final class TabPanelConfiguration extends AbstractComponentConfiguration 
 		this.tabs = new ArrayList<>();
 	}
 
+	/* (non-Javadoc)
+	 * @see name.martingeisse.guiserver.configuration.content.IConfigurationSnippet#setSnippetHandle(int)
+	 */
+	@Override
+	public void setSnippetHandle(int handle) {
+		this.snippetHandle = handle;
+	}
+	
+	/**
+	 * Getter method for the snippetHandle.
+	 * @return the snippetHandle
+	 */
+	public int getSnippetHandle() {
+		return snippetHandle;
+	}
+
 	/**
 	 * Adds a tab.
 	 * 
@@ -54,6 +76,14 @@ public final class TabPanelConfiguration extends AbstractComponentConfiguration 
 	public void addTab(TabEntry tabEntry) {
 		tabs.add(tabEntry);
 	}
+	
+	/**
+	 * Getter method for the tabs.
+	 * @return the tabs
+	 */
+	List<TabEntry> getTabs() {
+		return tabs;
+	}
 
 	/* (non-Javadoc)
 	 * @see name.martingeisse.guiserver.configuration.content.ComponentConfiguration#buildComponent()
@@ -61,32 +91,7 @@ public final class TabPanelConfiguration extends AbstractComponentConfiguration 
 	@Override
 	public Component buildComponent() {
 		final WebMarkupContainer tabPanelContainer = new WebMarkupContainer(getId() + "-container");
-		final PageParameterDrivenTabPanel tabPanel = new PageParameterDrivenTabPanel(getId(), parameterName) {
-			
-			@Override
-			protected Component createBody(String id, String selector) {
-				for (TabEntry tabEntry : tabs) {
-					AbstractTabInfo tabInfo = tabEntry.getTabInfo();
-					if (tabInfo instanceof TabInfo) {
-						if (selector.equals(((TabInfo)tabInfo).getSelector())) {
-							return tabEntry.buildTabEntry(id, tabPanelContainer);
-						}
-					} else {
-						throw new RuntimeException("cannot handle AbstractTabInfo objects other than TabInfo right now");
-					}
-				}
-				return new EmptyPanel(id);
-			}
-			
-			/* (non-Javadoc)
-			 * @see org.apache.wicket.markup.html.panel.Panel#newMarkupSourcingStrategy()
-			 */
-			@Override
-			protected IMarkupSourcingStrategy newMarkupSourcingStrategy() {
-				return new PanelMarkupSourcingStrategy(true);
-			}
-			
-		};
+		final PageParameterDrivenTabPanel tabPanel = new MyTabPanel(getId(), parameterName, this, tabPanelContainer);
 		for (TabEntry tab : tabs) {
 			tabPanel.addTab(tab.getTabInfo());
 		}
@@ -138,7 +143,7 @@ public final class TabPanelConfiguration extends AbstractComponentConfiguration 
 		protected MarkupContainer buildContainer() {
 			throw new UnsupportedOperationException();
 		}
-		
+
 		/**
 		 * Builds the component for a TabEntry.
 		 * 
@@ -151,4 +156,78 @@ public final class TabPanelConfiguration extends AbstractComponentConfiguration 
 		}
 
 	}
+
+	/**
+	 * Specialized tab panel implementation.
+	 */
+	public static final class MyTabPanel extends PageParameterDrivenTabPanel {
+		
+		/**
+		 * the markupProvider
+		 */
+		private final WebMarkupContainer markupProvider;
+
+		/**
+		 * the snippetHandle
+		 */
+		private final int snippetHandle;
+		
+		/**
+		 * the cachedTabPanelConfiguration
+		 */
+		private transient TabPanelConfiguration cachedTabPanelConfiguration;
+		
+		/**
+		 * Constructor.
+		 * @param id the wicket id
+		 * @param parameterName the name of the page parameter that contains the tab selector
+		 * @param tabPanelConfiguration the configuration object
+		 * @param markupProvider the component that contains the markup fragments for the tabs
+		 */
+		public MyTabPanel(String id, String parameterName, TabPanelConfiguration tabPanelConfiguration, WebMarkupContainer markupProvider) {
+			super(id, parameterName);
+			this.snippetHandle = tabPanelConfiguration.getSnippetHandle();
+			this.cachedTabPanelConfiguration = tabPanelConfiguration;
+			this.markupProvider = markupProvider;
+		}
+
+		/**
+		 * Getter method for the tab panel configuration.
+		 * @return the tab panel configuration.
+		 */
+		public final TabPanelConfiguration getTabPanelConfiguration() {
+			if (cachedTabPanelConfiguration == null) {
+				cachedTabPanelConfiguration = (TabPanelConfiguration)Configuration.getInstance().getSnippet(snippetHandle);
+			}
+			return cachedTabPanelConfiguration;
+		}
+		
+		/* (non-Javadoc)
+		 * @see name.martingeisse.wicket.component.misc.PageParameterDrivenTabPanel#createBody(java.lang.String, java.lang.String)
+		 */
+		@Override
+		protected Component createBody(String id, String selector) {
+			for (TabEntry tabEntry : getTabPanelConfiguration().getTabs()) {
+				AbstractTabInfo tabInfo = tabEntry.getTabInfo();
+				if (tabInfo instanceof TabInfo) {
+					if (selector.equals(((TabInfo)tabInfo).getSelector())) {
+						return tabEntry.buildTabEntry(id, markupProvider);
+					}
+				} else {
+					throw new RuntimeException("cannot handle AbstractTabInfo objects other than TabInfo right now");
+				}
+			}
+			return new EmptyPanel(id);
+		}
+
+		/* (non-Javadoc)
+		 * @see org.apache.wicket.markup.html.panel.Panel#newMarkupSourcingStrategy()
+		 */
+		@Override
+		protected IMarkupSourcingStrategy newMarkupSourcingStrategy() {
+			return new PanelMarkupSourcingStrategy(true);
+		}
+
+	}
+
 }
